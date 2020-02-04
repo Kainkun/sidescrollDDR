ec73e9a8cc8e6c10cf33af85a11657f91df3e05a
Kain Shepard <kainkun100@gmail.com>
2020-02-04 02:45:55 -0800

    unity project

---
+++ SideScrollingDDR/Assets/MainCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    Player playerScript;
+
+    public float cameraSpeed = 1;
+
+    private void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        if (Player.instance)
+            playerScript = Player.instance;
+        else
+            Debug.LogError("No Player");
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    Vector3 camGoTo;
+    private void FixedUpdate()
+    {
+        camGoTo = Vector2.Lerp(transform.position, playerScript.transform.position, Time.deltaTime * cameraSpeed);
+        camGoTo.z = -10;
+        transform.position = camGoTo;
+    }
+}
---
+++ SideScrollingDDR/Assets/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance;
+
+    Vector2 leftDirection;
+    Vector2 rightDirection;
+
+    public float speedForce = 1;
+    public float jumpForce = 1;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        instance = this;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        leftDirection.x = Input.GetAxis("LHorizontal");
+        leftDirection.y = Input.GetAxis("LVertical");
+
+        rightDirection.x = Input.GetAxis("RHorizontal");
+        rightDirection.y = Input.GetAxis("RVertical");
+
+        rb.AddForce(leftDirection * Vector2.right * speedForce);
+
+        if (Input.GetKeyDown(KeyCode.W))
+            Jump();
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+}
