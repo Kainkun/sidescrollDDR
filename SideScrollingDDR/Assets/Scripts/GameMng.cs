a6a0f98e1d3a2b2790eb23e042e03a37bc7dc327
Kain Shepard <kainkun100@gmail.com>
2020-02-24 20:53:55 -0800

    TMP, UI, Enemy radius, player damage

---
+++ SideScrollingDDR/Assets/Scripts/GameMng.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMng : MonoBehaviour
+{
+    public float interval = 0.33f;
+    public GameObject enemy;
+    bool spawning;
+    Player player;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        player = Player.instance;
+        spawning = true;
+        StartCoroutine(SpawnLoop());
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnLoop()
+    {
+        while (spawning)
+        {
+            yield return new WaitForSeconds(interval);
+
+            Vector2 spawnPos = GetSpawnPosition();
+            if(spawnPos != Vector2.zero)
+                Instantiate(enemy, (Vector2)player.transform.position + spawnPos, Quaternion.identity);
+        }
+        yield return null;  
+    }
+
+    Vector2 GetSpawnPosition()
+    {
+        Vector2 spawnDirection = Random.insideUnitCircle.normalized;
+        RaycastHit2D ray = Physics2D.Raycast(player.transform.position, spawnDirection, 10);
+        Debug.DrawRay(player.transform.position, spawnDirection * 10);
+
+        if (ray.collider == null)
+            return spawnDirection * 10;
+        else
+        {
+            if (ray.distance < 2)
+                return Vector2.zero;
+            else
+                return spawnDirection * (ray.distance - 2);
+        }
+
+
+    }
+
+}
---
+++ SideScrollingDDR/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    Player player;
+
+    public LineRenderer TRlines;
+    public LineRenderer BRlines;
+    public LineRenderer BLlines;
+    public LineRenderer TLlines;
+
+    public float cameraSpeed = 1;
+
+    Camera cam;
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Start()
+    {
+        if (Player.instance)
+            player = Player.instance;
+        else
+            Debug.LogError("No Player");
+
+        transform.position = player.transform.position;
+    }
+
+    private void Update()
+    {
+        SetLines();
+    }
+
+    void SetLines()
+    {
+        TRlines.SetPosition(0, player.transform.position - transform.position);
+        BRlines.SetPosition(0, player.transform.position - transform.position);
+        BLlines.SetPosition(0, player.transform.position - transform.position);
+        TLlines.SetPosition(0, player.transform.position - transform.position);
+
+        TRlines.SetPosition(1, cam.ViewportToWorldPoint(new Vector2(1, 1)) - transform.position);
+        BRlines.SetPosition(1, cam.ViewportToWorldPoint(new Vector2(1, 0)) - transform.position);
+        BLlines.SetPosition(1, cam.ViewportToWorldPoint(new Vector2(0, 0)) - transform.position);
+        TLlines.SetPosition(1, cam.ViewportToWorldPoint(new Vector2(0, 1)) - transform.position);
+    }
+
+
+    Vector3 camGoTo;
+    private void FixedUpdate()
+    {
+        camGoTo = Vector2.Lerp(transform.position, player.transform.position, Time.deltaTime * cameraSpeed);
+        camGoTo.z = -10;
+        transform.position = camGoTo;
+    }
+}
